37637a5f2c4cf53f56bdd7d050a66f343d1fc371
lucaswatanuki <lucaswatanuki@gmail.com>
2020-01-30 20:51:57 -0300

    Controller classes add

--- src/main/java/com/backend/rcs/controller/UserController.java
+++ src/main/java/com/backend/rcs/controller/UserController.java
@@ -1,10 +1,37 @@
 package com.backend.rcs.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.backend.rcs.controller.request.UserRequest;
+import com.backend.rcs.controller.response.UserResponse;
+import com.backend.rcs.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("")
+    public UserResponse post(@RequestBody UserRequest request){
+        return userService.save(request);
+    }
+
+    @GetMapping("/{id}")
+    public UserResponse getUserById(@PathVariable("id") String id){
+        return userService.findById(id);
+    }
+
+    @GetMapping("")
+    public List<UserResponse> getAll(){
+        return userService.findAllUsers();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") String id){
+        userService.delete(id);
+    }
 }
