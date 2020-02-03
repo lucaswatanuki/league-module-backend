6eb29f790dbbc99e89c0ce10d3539ffbbb8198b4
lucaswatanuki <lucaswatanuki@gmail.com>
2020-02-03 20:50:40 -0300

    Mappers classes remove + mongoDB uri connection

---
+++ src/main/java/com/backend/rcs/RcsApplication.java
@@ -0,0 +1,13 @@
+package com.backend.rcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcsApplication.class, args);
+	}
+
+}
